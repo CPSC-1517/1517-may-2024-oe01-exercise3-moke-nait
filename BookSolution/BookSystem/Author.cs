19886e602be69caeea8cfd21f10966ac8e442c52
moke-nait <moke1@nait.ca>
2024-07-05 20:49:42 -0600

    first draft

---
+++ BookSolution/BookSystem/Author.cs
@@ -0,0 +1,112 @@
+﻿using System.Text.RegularExpressions;
+
+
+namespace BookSystem
+{
+    public class Author
+    {
+        #region Data Members
+        private string _FirstName;
+        private string _LastName;
+        private string _ResidentCity;
+        private string _ResidentCountry;
+        private string _ContactUrl;
+        #endregion
+
+        #region Properties
+        public string FirstName
+        {
+            get { return _FirstName; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Firstname is required");
+                }
+                _FirstName = value.Trim();
+            }
+        }
+
+        public string LastName
+        {
+            get { return _LastName; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Lastname is required");
+                }
+                _LastName = value.Trim();
+            }
+        }
+
+        public string ResidentCity
+        {
+            get { return _ResidentCity; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Resident city is required");
+                }
+                _ResidentCity = value.Trim();
+            }
+        }
+
+        public string ResidentCountry
+        {
+            get { return _ResidentCountry; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Resident country is required");
+                }
+                _ResidentCountry = value.Trim();
+            }
+        }
+
+        public string ContactUrl
+        {
+            get { return _ContactUrl; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Contact URL is required");
+                }
+
+                string pattern = @"^(https?://)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(\/\S*)?$";
+                Regex regex = new Regex(pattern);
+
+                if (!regex.IsMatch(value))
+                {
+                    throw new ArgumentException("Contact URL does not have an acceptable url pattern");
+                }
+                _ContactUrl = value.Trim();
+            }
+        }
+
+        public string AuthorName
+        {
+            get
+            { return $"{LastName}, {FirstName}"; }
+        }
+        #endregion
+
+        #region Constructors
+        public Author (string firstname, string lastname, string contacturl, string city, string country)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            ContactUrl = contacturl;
+            ResidentCity = city;
+            ResidentCountry = country;
+        }
+        #endregion
+
+        #region Methods
+        public override string ToString() => $"{FirstName},{LastName},{ContactUrl},{ResidentCity},{ResidentCountry}";
+        #endregion
+    }
+}
---
+++ BookSolution/BookSystem/Reviewer.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BookSystem
+{
+    public class Reviewer
+    {
+        #region Data Members
+        private string _FirstName;
+        private string _LastName;
+        private string _Organization;
+        private string _Email;
+        #endregion
+
+        #region Properties
+        public string FirstName
+        {
+            get { return _FirstName; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("First name is required");
+                }
+                _FirstName = value.Trim();
+            }
+        }
+
+        public string LastName
+        {
+            get { return _LastName; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Last name is required");
+                }
+                _LastName = value.Trim();
+            }
+        }
+
+        public string Organization
+        {
+            get { return _Organization; }
+            set
+            {                
+                _Organization = value?.Trim();
+            }
+        }
+
+        public string Email
+        {
+            get { return _Email; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Email is required");
+                }
+
+                string emailPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+                Regex regex = new Regex(emailPattern);
+
+                if (!regex.IsMatch(value))
+                {
+                    throw new ArgumentException("Email is not an acceptable email address pattern");
+                }
+                _Email = value.Trim();
+            }
+        }
+
+        public string ReviewerName
+        {
+            get
+            {
+                return $"{LastName}, {FirstName}";
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+        public Reviewer (string firstname, string lastname, string email, string organization = null)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            Email = email;
+            Organization = organization;
+        }
+        #endregion
+
+        #region Methods
+        public override string ToString() => $"{FirstName},{LastName},{Email},{Organization}";        
+        #endregion
+    }
+    }
